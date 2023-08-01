319db746a6a0773c4fd82e94d2729e1593dbe44b
unknown <Minskoleopc>
2023-08-01 06:55:22 -0700

    added

---
+++ script21.js
@@ -0,0 +1,133 @@
+let firstName = "chinmay"
+console.log(firstName)
+console.log(typeof firstName)
+
+// Human 
+// Properties --- color , gender , height weight 
+// Method - walk() , talk()
+
+
+
+
+
+
+// Object --- 
+// Properties and methods
+
+// property - length 
+let city = "pune"
+console.log(city.length) // 4
+
+
+// toUpperCase()
+let city2 = "india"
+let city2a = city2.toUpperCase()
+console.log(city2a)
+
+// toLowerCase()
+let city3 = "Bhopal"
+let city4 = city3.toLowerCase()
+console.log(city4)
+
+// includes()
+
+let city5 = "indore"
+let q1 = city5.includes('i')
+console.log(q1)
+
+let q2 = city5.includes('ind')
+console.log(q2)
+
+// indexOf()
+//   0      1     2    3    4    5  6 
+//   j      a     i    p    u    r  a
+let city6 = "jaipura"
+let q3 = city6.indexOf('a',2)
+console.log(q3)
+
+let q4 = city6.indexOf('A')
+console.log(q4)
+
+
+let city7 = "nagpur"
+let q5 = city7.startsWith('n')
+let q6 = city7.startsWith('nag')
+let q7 = city7.startsWith('r')
+console.log(q5)
+console.log(q6)
+console.log(q7)
+
+
+let city8 = 'udaipur'
+let q8 =city8.endsWith('r')
+let q9 =city8.endsWith('pur')
+let q10 =city8.endsWith('puR')
+console.log(q8)
+console.log(q9)
+console.log(q10)
+
+
+let sentence = "I am learning javascript"
+let q11 = sentence.replace("javascript","python")
+console.log(q11)
+
+let city11 = " goa"
+console.log(city11.length)
+let q12 = city11.trimStart()
+console.log(q12.length)
+
+let city12 = " goa "
+console.log(city12.length)
+let q13 = city12.trimEnd()
+console.log(q13.length)
+
+
+let city13 = " goa "
+console.log(city13.length)
+let q14 = city13.trim()
+console.log(q14.length)
+
+// toUpperCase()
+// toLowerCase()
+// includes()
+// indexOf()
+// startsWith()
+// endsWith()
+// trim()
+// trimStart()
+// trimEnd()
+// replace()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
